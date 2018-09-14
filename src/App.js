5812f6ca1f50ab5e0e05781038f43acfb492cd57
biigpongsatorn <biig_pongsatorn@hotmail.com>
2018-09-14 18:19:42 +0700

    vue custom element completely

--- src/App.js
+++ src/App.js
@@ -14,6 +14,17 @@ class App extends Component {
           <img src={angularLogo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Micro frontend 101</h1>
         </header>
+        <div className="is-expanded">
+          <div className="is-33pct">
+            <vue-data-table str="สวัสดี Vue.js" num={456} bool={false} obj="{'name': 'Pongsatorn'}" arr={[4, 5, 6]}/>
+          </div>
+          <div className="is-33pct">
+            React Custom Element coming soon...
+          </div>
+          <div className="is-33pct">
+            Angular Custom Element coming soon...
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/custom-element/vue/src/components/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div>
+      {{ message }}
+    </div>
+    <table border="1">
+      <thead>
+        <tr>
+          <td>
+            <b>Type</b>
+          </td>
+          <td>
+            <b>Value</b>
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>String</td>
+          <td>{{ str }}</td>
+        </tr>
+        <tr>
+          <td>Number</td>
+          <td>{{ num }}</td>
+        </tr>
+        <tr>
+          <td>Boolean</td>
+          <td>{{ bool }}</td>
+        </tr>
+        <tr>
+          <td>Object</td>
+          <td>{{ obj }}</td>
+        </tr>
+        <tr>
+          <td>Array</td>
+          <td>{{ arr }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'vue-data-table',
+  data () {
+    return {
+      message: 'Vue Custom Element'
+    }
+  },
+  props: {
+    str: {
+      type: String,
+      default: 'Hello Vue'
+    },
+    num: {
+      type: Number,
+      default: 123
+    },
+    bool: {
+      type: Boolean,
+      default: true
+    },
+    obj: {
+      type: Object,
+      default: () => {
+        return { key: 'value' }
+      }
+    },
+    arr: {
+      type: Array,
+      default: () => [1, 2, 3]
+    }
+  }
+}
+</script>
