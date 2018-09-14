f3520bddd3ab86603d97df9831db6a828954cad5
biigpongsatorn <biig_pongsatorn@hotmail.com>
2018-09-14 16:12:19 +0700

    add logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import reactLogo from './assets/react-logo.svg'
+import vueLogo from './assets/vue-logo.png'
+import angularLogo from './assets/angular-logo.png'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={reactLogo} className="App-logo" alt="logo"/>
+          <img src={vueLogo} className="App-logo" alt="logo"/>
+          <img src={angularLogo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Micro frontend 101</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    )
   }
 }
 
